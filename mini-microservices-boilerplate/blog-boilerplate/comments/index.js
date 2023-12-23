d10144988934d50e8a0f703b615d715359181eab
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-23 06:46:26 +0200

    adding bus events

--- mini-microservices-boilerplate/blog-boilerplate/comments/index.js
+++ mini-microservices-boilerplate/blog-boilerplate/comments/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 
@@ -11,7 +12,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
@@ -20,6 +21,15 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id: commentId, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "Comment Created",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
