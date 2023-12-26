a5b7a524c6b2c7bd12d60f0bb7eb1237136832c9
abdelslam_elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-12-26 18:15:04 +0200

    update

--- mini-microservices-boilerplate/blog-boilerplate/query/index.js
+++ mini-microservices-boilerplate/blog-boilerplate/query/index.js
@@ -5,12 +5,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const posts = {};
+
 app.get("/events", (req, res) => {});
 
 app.post("/events", (req, res) => {
   const { type, data } = req.body;
   if (type == "postCreated") {
     const { title, id } = data;
+
+    posts[id] = { title, id, comments: [] };
   } else if (type == "commentCreated") {
     const { id, content, postId } = data;
   }
