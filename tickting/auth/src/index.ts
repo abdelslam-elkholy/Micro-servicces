f3fe44887f2e8225123349c35392c040d3a5d64b
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-01-26 20:13:01 +0200

    update

--- tickting/auth/src/index.ts
+++ tickting/auth/src/index.ts
@@ -3,6 +3,9 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+app.get("/api/users", (req, res) => {
+  res.send("Hi There");
+});
 app.listen(3000, () => {
   console.log("Listening on Port: 3000 !");
 });
