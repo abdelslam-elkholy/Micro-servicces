d0b8a34f6a045cd1b20bfcf56f724914db70bb78
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-01-26 21:11:40 +0200

    update

--- tickting/auth/src/index.ts
+++ tickting/auth/src/index.ts
@@ -1,11 +1,10 @@
 import express from "express";
-
+import { currentUserRouter } from "./routes/current-user";
 const app = express();
 app.use(express.json());
 
-app.get("/api/users", (req, res) => {
-  res.send("Hi There");
-});
+app.use(currentUserRouter);
+
 app.listen(3000, () => {
   console.log("Listening on Port: 3000 !");
 });
--- tickting/auth/src/routes/current-user.ts
+++ tickting/auth/src/routes/current-user.ts
@@ -2,6 +2,8 @@ import express from "express";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {});
+router.get("/api/users/currentuser", (req, res) => {
+  res.send("<h1>Welcome!</h1>");
+});
 
 export { router as currentUserRouter };
