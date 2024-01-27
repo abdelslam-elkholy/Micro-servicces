481da5436d23b4a1fbdcbb55173855ab8b7c8f87
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-01-27 22:30:36 +0200

    update

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -8,13 +8,14 @@ import { signupRouter } from "./routes/signup";
 const app = express();
 app.use(express.json());
 
-app.get("/api/", (req: Request, res: Response) => {
+app.get("/api/users/h", (req: Request, res: Response) => {
   res.send("Hey");
 });
+
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
-app.use(signupRouter);
+app.use("/api/users/signup", signupRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000!!!");
