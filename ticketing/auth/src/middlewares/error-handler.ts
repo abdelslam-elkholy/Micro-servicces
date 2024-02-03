4f33aba0b3b01ca264b4864263038788c82b7f24
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-02-03 23:00:58 +0200

    handling app errors

--- ticketing/auth/src/middlewares/error-handler.ts
+++ ticketing/auth/src/middlewares/error-handler.ts
@@ -8,6 +8,7 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof CustomError) {
+    return res.status(err.statusCode).json({ errors: err.serializeErrors() });
   }
 
   res.status(400).json({ error: err.message });
