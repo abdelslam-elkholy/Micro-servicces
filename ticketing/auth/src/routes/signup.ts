c1e17a9bb06060529ff5f271e5c89504927528d5
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-03-11 23:25:11 +0200

    update

--- ticketing/auth/src/routes/signup.ts
+++ ticketing/auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 import { RequstValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { IUser, User } from "../models/user";
 
 const router = express.Router();
 
@@ -14,7 +14,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -22,12 +22,13 @@ router.post(
     }
     const { email, password } = req.body;
 
-    console.log("Creating a user...");
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      console.log("Email already in Use");
+      return res.send({});
+    }
 
-    // throw new DatabaseConnectionError();
-    res.send({});
-
-    // new User({ email, password })
+    const user = User.create({ email, password });
   }
 );
 
