4889e7e2d907d8a75e3adb4721f9ab2fa3dd93a9
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-04-07 23:34:15 +0200

    update

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import jwt from "jsonwebtoken";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
+router.get("/api/users/current-user", (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.jwt as string);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
