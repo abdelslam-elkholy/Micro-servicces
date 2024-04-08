9a7beb0d5e6cb532553b1b7640e75fb7ba7b8b8c
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-04-08 02:00:53 +0200

    add auth middleware

--- ticketing/auth/src/routes/current-user.ts
+++ ticketing/auth/src/routes/current-user.ts
@@ -1,14 +1,16 @@
 import { currentUser } from "./../middlewares/current-user";
 import jwt from "jsonwebtoken";
 import express, { Request, Response } from "express";
+import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
 router.get(
   "/api/users/current-user",
   currentUser,
+  requireAuth,
   (req: Request, res: Response) => {
-    res.send({ currenUser: req.currentUser });
+    res.send({ currenUser: req.currentUser || null });
   }
 );
 
