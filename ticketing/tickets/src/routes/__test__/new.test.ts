71c7d537ca0c70f8d510af8dac6e1a711ce70e6d
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-05-05 20:53:06 +0300

    adding test suit for tickets service

---
+++ ticketing/tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,7 @@
+import request from "supertest";
+
+it("has a route handler listening to /api/tickets for post requests", async () => {});
+it("returns an error if invalid title is provided ", async () => {});
+it("returns an error if invalid price is provided ", async () => {});
+it("can only be accessed if the user is loggedin ", async () => {});
+it("creats tickets with valid inputs ", async () => {});
