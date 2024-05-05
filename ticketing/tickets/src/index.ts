0aa293cce1bc6b43ea50cbddce54ac53218c5af5
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-05-05 20:38:45 +0300

    updating depl yaml files

--- ticketing/tickets/src/index.ts
+++ ticketing/tickets/src/index.ts
@@ -5,8 +5,11 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY is required");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI is required");
+  }
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected Successfully To DB");
   } catch (error) {
     console.log(error);
