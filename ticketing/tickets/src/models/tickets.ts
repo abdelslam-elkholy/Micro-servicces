c4e03c2bed775083a19908e39768cc27305dfc16
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2024-05-06 20:45:19 +0300

    designing tickets model

---
+++ ticketing/tickets/src/models/tickets.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+interface TicketsAttrs {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketsAttrs): TicketDoc;
+}
